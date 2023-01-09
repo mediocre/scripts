4e22dfe9d02ac692f0fbf850db61996079613c90
Shawn Miller <shawn@mediocre.com>
2023-01-08 23:42:01 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
-eval $(/opt/homebrew/bin/brew shellenv)
+#echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
+#eval $(/opt/homebrew/bin/brew shellenv)
 
 # AWS CLI
 brew install awscli
