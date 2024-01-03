949a6fe5f6d3bcde80c3b0ac15d82bf05068966c
Shawn Miller <shawn@mediocre.com>
2024-01-02 19:34:38 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -26,7 +26,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 20
+nvm install 18
 
 # Downgrade to NPM 6
 npm install -g npm@6
