1f48a62ec581c9c10e640373a0f29252146cf9ef
Shawn Miller <shawn@mediocre.com>
2024-01-02 22:13:15 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 # This is for Mac computers with Apple silicon: https://support.apple.com/en-us/HT211814
 
 # Rosetta 2
-#/usr/sbin/softwareupdate --install-rosetta --agree-to-license
+/usr/sbin/softwareupdate --install-rosetta --agree-to-license
 
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
