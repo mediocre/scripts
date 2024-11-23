9407e29c1b5b3623b3fff1617d9fb8f2a3803557
Shawn Miller <shawn@mediocre.com>
2024-11-22 23:31:56 -0600

    Create kiosk5.sh

---
+++ kiosk5.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Install updates
+sudo apt update -y
+sudo apt -qq upgrade -y
+sudo apt -qq autoremove -y > /dev/null
+sudo apt install -y x11-xserver-utils unclutter
+
+AUTOSTART_PATH=$HOME/.config/lxsession/LXDE-pi
+mkdir -p $AUTOSTART_PATH
+
+cat > $AUTOSTART_PATH/kiosk.sh <<EOF
+#!/bin/bash
+
+# Turn off screensaver and disable energysaver
+xset s off
+xset s noblank
+
+while true;
+do
+  # Remove the mouse cursor after 10 seconds of idleness
+  unclutter -idle 10 -grab &
+
+  # Ensure that if we have a power cut or bad shutdown that the chromium preferences are reset to a "good" state so we don't get the restore previous session dialog
+  sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' $HOME/.config/chromium/Default/Preferences
+  sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' $HOME/.config/chromium/Default/Preferences
+
+  # Kill all Chromium processes
+  pkill -o chromium
+
+  chromium-browser --autoplay-policy=no-user-gesture-required --disable-background-timer-throttling --disable-extensions --disable-features=PowerSaverMode --disable-infobars --disable-media-stream --disable-renderer-backgrounding --hide-scrollbars --kiosk --no-default-browser-check --noerrdialogs --start-fullscreen \
+  $1 &
+
+  # Restart Chromium after an hour
+  sleep 3600
+done
+EOF
+
+chmod +x $AUTOSTART_PATH/kiosk.sh
+
+if [[ ! -f $AUTOSTART_PATH/autostart ]]; then
+  # We are clear to clone the current autostart
+  cp /etc/xdg/lxsession/LXDE-pi/autostart $AUTOSTART_PATH/autostart
+  echo "@$AUTOSTART_PATH/kiosk.sh" >> $AUTOSTART_PATH/autostart
+else
+  if grep -Fxq "@$AUTOSTART_PATH/kiosk.sh" $AUTOSTART_PATH/autostart; then
+    echo "[SKIP] kiosk already setup"
+  else
+    echo "@$AUTOSTART_PATH/kiosk.sh" >> $AUTOSTART_PATH/autostart
+  fi
+fi
+
+sudo reboot
