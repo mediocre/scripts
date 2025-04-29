2b547b8bd9ce85b03e2d818674afddd68e374e43
Shawn Miller <shawn@mediocre.com>
2025-04-28 21:15:50 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,3 @@
-# This is for Mac computers with Apple silicon: https://support.apple.com/en-us/HT211814
-
-# Rosetta 2
-#/usr/sbin/softwareupdate --install-rosetta --agree-to-license
-
 # Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
@@ -13,19 +8,19 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # MongoDB
 brew tap mongodb/brew
-brew install mongodb-community@7.0
-brew services start mongodb-community@7.0
+brew install mongodb-community@8.0
+brew services start mongodb-community@8.0
 
 # Redis
 brew install redis
 brew services start redis
 
 # NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 20
+nvm install 22
 
 # Downgrade to NPM 6
 npm install -g npm@6
@@ -54,10 +49,11 @@ brew install ngrok
 brew install visual-studio-code
 
 # Visual Studio Code Extensions
-#code --install-extension dbaeumer.vscode-eslint
-#code --install-extension DigitalBrainstem.javascript-ejs-support
-#code --install-extension GitHub.copilot
-#code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension DigitalBrainstem.javascript-ejs-support
+code --install-extension GitHub.copilot
+code --install-extension GitHub.copilot-chat
+code --install-extension ms-vsliveshare.vsliveshare
 
 # Mac App Store CLI
 brew install mas
